5d7f64e6dc3a262b872e1023d3507b6a476c4f18
amantechDevise <amantechdevise@gmail.com>
2025-07-19 14:07:55 +0530

    add

--- src/app/IT_Staff/page.tsx
+++ src/app/IT_Staff/page.tsx
@@ -7,7 +7,6 @@ function IT_Staff() {
     return (
         <Layout>
             <main className="relative w-full h-full">
-
                 <div className="relative w-full h-[600px] md:h-[750px] md:pb-[120px] overflow-hidden">
                     {/* Background Video */}
                     <img
@@ -17,7 +16,17 @@ function IT_Staff() {
 
 
                     <div className="relative z-10 h-full flex flex-col justify-center items-center text-center">
+                        <h1 className="text-2xl md:text-[50px] font-bold font-raleway text-white leading-tight md:mb-[20px] mb-0 ">
+                            Your Trusted Partner for <br />
+                            IT Staffing and Services
+                        </h1>
+                        <p className="text-white md:w-3xl mt-10 text-[12px] md:text-[16px] text-center">
+                            PlacerIT is a staffing agency that has the expertise, experience, and network to find you the most qualified candidates for any industry or function in as little as two days. Plus, we have access to top international talent and can source great people from 50+ countries.
+                            Questions?
 
+
+
+                        </p>
                         <br />
                         <p className="text-[12px] md:text-[16px] text-white"> Call us toll-free: +1 123 456 7890</p>
 
